125406e9dc5dd974b62605f5658eeb67f8568f83
packa <amb258012@gmail.com>
2021-01-07 21:08:28 +0900

    feat: 게시글 목록 조회 기능 추가

--- list.php
+++ list.php
@@ -1,17 +1,40 @@
+<?php
+    include "mysqlConnect.php";
+
+    
+?>
+
 <meta charset="utf-8">
 <title>게시판</title>
 
     <h2>게시글 리스트</h2>
-    <table>
+    <table width=900>
         <tr>
             <th>번호</th>
             <th>제목</th>
             <th>작성자</th>
+            <th>내용</th>
             <th>작성일</th>
         </tr>
-        <tr>
-            <td></td>
-        </tr>
+
+    <?
+        $sql = "select * from php_board order by id desc";
+        $result = mysqli_query($connect, $sql);
+        while($row = mysqli_fetch_array($result)){
+    ?>
+            <tr>
+                <td> <?=$row['id'] ?></td>
+                <td> <?=$row['title'] ?></td>
+                <td> <?=$row['name'] ?></td>
+                <td> <?=$row['content'] ?></td>
+                <td> <?=$row['regdate'] ?></td>
+
+            </tr>
+    
+    
+    <?   } 
+    ?>
+
     </table>
     <a href="newPost.php">글 쓰기</a>
 
