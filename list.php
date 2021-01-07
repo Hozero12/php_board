61933d6a5fa8ff12b11dd81d6041ab9db4245dde
packa <amb258012@gmail.com>
2021-01-07 15:48:39 +0900

    글 리스트 페이지 구성, 글 쓰기 링크 추가

--- list.php
+++ list.php
@@ -1,9 +1,20 @@
-<head>
+<meta charset="utf-8">
+<title>게시판</title>
+
+    <h2>게시글 리스트</h2>
+    <table>
+        <tr>
+            <th>글 번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일</th>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+    <a href="newPost.php">글 쓰기</a>
 
 
-</head>
-
-<body>
 
 
-</body>
