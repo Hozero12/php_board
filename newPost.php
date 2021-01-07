62f59f28d667bccd47a0b2a3b45b693d248ccb5f
packa <amb258012@gmail.com>
2021-01-07 16:28:14 +0900

    '새 글 작성' 페이지 구성, list.php수정

---
+++ newPost.php
@@ -0,0 +1,11 @@
+<h2>새 글 작성</h2>
+
+<form action="post.php" method="post">
+    제목 
+    <input type="text" name="title" cols=20>
+    작성자
+    <input type="text" name="name" cols=10><br>
+    내용 
+    <textarea name="title" cols=100 rows=20></textarea><br><br>
+    <input type="submit" value="작성">
+</form>
