e6191faa35c436c5ce0b79273338322974587bab
packa <amb258012@gmail.com>
2021-01-07 21:07:32 +0900

    fix: sql 사용자 정보 수정

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -1,8 +1,8 @@
 <?php
 
-    $connect = mysqli_connect("localhost","hoyoung","dt1210","board");
+    $connect = mysqli_connect('localhost','root','dt1210','board');
     
-
+    
     if(mysqli_connect_error()){
         echo "mysql 접속 실패";
         echo mysqli_connect_error();
