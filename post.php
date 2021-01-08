08860d2c7a5d32b66795ba89646aa4ab0cb497e8
packa <amb258012@gmail.com>
2021-01-08 14:17:22 +0900

    feat: 사용자 비밀번호 DB저장 기능 추가

--- post.php
+++ post.php
@@ -4,15 +4,20 @@
     $title = $_POST['title'];
     $name = $_POST['name'];
     $content = $_POST['content'];
-
+   
+    
     $title = mysqli_real_escape_string($connect, $title);
     $name = mysqli_real_escape_string($connect, $name);
     $content = mysqli_real_escape_string($connect, $content);
     $regdate = date("Y-m-d H:i:s");
-
-    $sql = "insert into php_board(title, name, content, regdate) values('$title', '$name', '$content', '$regdate')";
+   
+    if(isset($_POST['lock']) ){
+        $password = password_hash(mysqli_real_escape_string($connect, $_POST['pw']), PASSWORD_DEFAULT);
+        $sql = "insert into php_board(title, name, content, regdate, pwd) values('$title', '$name', '$content', '$regdate', '$password')";
+    }else{
+        $sql = "insert into php_board(title, name, content, regdate) values('$title', '$name', '$content', '$regdate')";
+    }
     mysqli_query($connect, $sql);
-
 ?>
 
 <script>
